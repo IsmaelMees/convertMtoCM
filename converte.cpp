743ab258a7acd26a2ddfd52c2949dbbcc56292d6
IsmaelMees <93687763+IsmaelMees@users.noreply.github.com>
2021-11-03 21:00:25 -0300

    Add files via upload

---
+++ converte.cpp
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main()
+
+{
+
+
+float m, cm;
+printf("Bem vindo ao conversor online.\n digite o valor a ser convertido em metros (M) \n");
+		scanf("%f",&m);
+cm = (m * 100);
+printf(" %f metros sao %f centimetros",m,cm);
+
+	return 0;
+}
